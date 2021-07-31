018c93314d209a95ece6b6a52d25ee9eb08ab5f7
arlealexandre <70630831+arlealexandre@users.noreply.github.com>
2021-07-31 11:42:27 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package package_main;
+
+import javax.script.ScriptException;
+import package_controleur.Controleur;
+
+
+public class Main {
+
+    public static void main(String[] args) throws ScriptException {
+        new Controleur();
+    }
+    
+}
